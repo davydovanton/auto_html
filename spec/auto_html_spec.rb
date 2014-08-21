ac01217b5862934f3083005c496e9cb32623c208
Anton Davydov <don.mist@gmail.com>
2014-08-22 01:56:19 +0400

    Update auto_html_spec file

--- spec/auto_html_spec.rb
+++ spec/auto_html_spec.rb
@@ -1,35 +1,44 @@
 require 'spec_helper'
 
 describe 'AutoHtml' do
-end
+  it 'should be raw input when no filters provided' do
+    input = 'Hey check out my blog => http://rors.org'
+    expect(auto_html(input){}).to eq input
+  end
 
-# def test_should_be_raw_input_when_no_filters_provided
-#   input = 'Hey check out my blog => http://rors.org'
-#   result = auto_html(input) { }
-#   assert_equal result, input
-# end
-#
-# def test_should_apply_simple_format_filter
-#   result = auto_html('Hey check out my blog => http://rors.org') { simple_format }
-#   assert_equal '<p>Hey check out my blog => http://rors.org</p>', result
-# end
-#
-# def test_should_apply_simple_format_and_image_filter
-#   result = auto_html('Check the logo: http://rors.org/images/rails.png') { simple_format; image(alt: nil) }
-#   assert_equal '<p>Check the logo: <img src="http://rors.org/images/rails.png" alt=""/></p>', result
-# end
-#
-# def test_should_apply_simple_format_image_and_link_filter
-#   result = auto_html('Check the logo: http://rors.org/images/rails.png. Visit: http://rubyonrails.org') { simple_format; image(alt: nil); link }
-#   assert_equal '<p>Check the logo: <img src="http://rors.org/images/rails.png" alt=""/>. Visit: <a href="http://rubyonrails.org" >http://rubyonrails.org</a></p>', result
-# end
-#
-# def test_should_return_blank_if_input_is_blank
-#   result = auto_html('') { simple_format; image(alt: nil); link }
-#   assert_equal '', result
-# end
-#
-# def test_should_not_apply_simple_format_if_input_is_nil
-#   result = auto_html(nil) { simple_format; image(alt: nil); link }
-#   assert_equal '', result
-# end
+  it 'should apply simple format filter' do
+    input = 'Hey check out my blog => http://rors.org'
+    expect(auto_html(input){ simple_format }).to eq "<p>#{input}</p>"
+  end
+
+  it 'should apply simple format and image filter' do
+    result = '<p>Check the logo: <img src="http://rors.org/images/rails.png" alt=""/></p>'
+    expect(
+      auto_html('Check the logo: http://rors.org/images/rails.png') { simple_format; image(alt: nil) }
+    ).to eq result
+  end
+
+  it 'should apply simple format image and link filter' do
+    result = '<p>Check the logo: <img src="http://rors.org/images/rails.png" alt=""/>. Visit: <a href="http://rubyonrails.org" >http://rubyonrails.org</a></p>'
+
+    expect(
+      auto_html('Check the logo: http://rors.org/images/rails.png. Visit: http://rubyonrails.org') {
+        simple_format;
+        image(alt: nil);
+        link
+      }
+    ).to eq result
+  end
+
+  it 'should return blank if input is blank' do
+    expect(
+      auto_html('') { simple_format; image(alt: nil); link }
+    ).to eq ''
+  end
+
+  it 'should not apply simple format if input is nil' do
+    expect(
+      auto_html(nil) { simple_format; image(alt: nil); link }
+    ).to eq ''
+  end
+end
